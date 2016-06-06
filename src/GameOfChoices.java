0764eb7ef5a49ff5db7b98b94ae7adfaadbc2a4d
Jonathan Abdo <jabdo79@gmail.com>
2016-06-06 16:48:29 -0400

    First commit

---
+++ src/GameOfChoices.java
@@ -0,0 +1,23 @@
+/* Jonathan Abdo
+ * Create a Choose your own adventure game!
+ */
+
+import java.util.Scanner;
+
+public class GameOfChoices {
+	
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		//declare any variables needed
+		String name;
+		
+		System.out.println("Welcome to the Game of Thrones!!! err Choices!!!\n");
+		//take user info!
+		System.out.print("Enter your name: ");
+		name = sc.nextLine();
+		
+
+	}
+
+}
