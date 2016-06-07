745035407a3e9f52d96a09c259c3108b3220186e
Jonathan Abdo <jabdo79@gmail.com>
2016-06-07 09:46:23 -0400

    Add inputs for user info

--- src/GameOfChoices.java
+++ src/GameOfChoices.java
@@ -1,4 +1,5 @@
 /* Jonathan Abdo
+ * 6-6-2016
  * Create a Choose your own adventure game!
  */
 
@@ -9,15 +10,25 @@ public class GameOfChoices {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		
 		//declare any variables needed
-		String name;
+		String name, hair, house;
 		
 		System.out.println("Welcome to the Game of Thrones!!! err Choices!!!\n");
 		//take user info!
 		System.out.print("Enter your name: ");
 		name = sc.nextLine();
+		System.out.println("Enter your hair color: ");
+		hair = sc.nextLine();
+		System.out.println("Choose a house (Stark, Lannister or Targaryen)");
+		house = sc.nextLine();
 		
-
+		//send user to appropriate story arc for house chosen
+		
+	}
+	
+	public void stark(String name, String hair){
+		
 	}
 
 }
